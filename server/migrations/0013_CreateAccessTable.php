9630fe90737ef7894f2fad91405a7f02ad192928
Zontreck <tarapiccari@gmail.com>
2026-04-07 23:19:43 -0700

    Create access table

---
+++ server/migrations/0013_CreateAccessTable.php
@@ -0,0 +1,9 @@
+<?php
+
+return function($conn) {
+    $conn->query("CREATE TABLE IF NOT EXISTS `AccessTokens` (User VARCHAR(64) UNIQUE, Token VARCHAR(64) UNIQUE, TokenScope int NOT NULL DEFAULT 0, TokenFlags int NOT NULL DEFAULT 0);");
+
+    $conn->query("ALTER TABLE `AccessTokens` ADD PRIMARY KEY(`User`);");
+}
+
+?>
