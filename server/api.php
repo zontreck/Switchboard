452ca81394b81713d12b060a6ce78841e763735a
Zontreck <tarapiccari@gmail.com>
2026-04-11 13:50:20 -0700

    Fix foreign key additions

--- server/api.php
+++ server/api.php
@@ -2,7 +2,7 @@
 
 $DEBUG = true;
 
-$VERSION = "0.1.041126+1316";
+$VERSION = "0.1.041126+1349";
 
 if(defined("MAINTENANCE")) {
     header("Content-Type: application/json");
--- server/migrations/0006_AddCascadeToAccess.php
+++ server/migrations/0006_AddCascadeToAccess.php
@@ -1,8 +1,6 @@
 <?php
 
 return function($conn) {
-    $conn->query("ALTER TABLE `users` CHANGE `ID` `ID` VARCHAR(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_uca1400_ai_ci NOT NULL;");
-    
     $conn->query("ALTER TABLE `Access` ADD CONSTRAINT `UID` FOREIGN KEY (`User`) REFERENCES `users`(`ID`) ON DELETE CASCADE ON UPDATE CASCADE;");
 }
 
