b089d01159e78df88763683abb9cd936a1123276
Zontreck <tarapiccari@gmail.com>
2026-04-10 18:35:24 -0700

    Fix a syntax error in migration

--- server/migrations/0016_ChangeAccessPrimaryKey.php
+++ server/migrations/0016_ChangeAccessPrimaryKey.php
@@ -1,7 +1,7 @@
 <?php
 
 return function ($conn) {
-    $conn->query("ALTER TABLE `AccessTokens` DROP PRIMARY KEY(`User`);");
+    $conn->query("ALTER TABLE `AccessTokens` DROP PRIMARY KEY;");
     $conn->query("ALTER TABLE `AccessTokens` ADD PRIMARY KEY(`Token`);");
 }
 
