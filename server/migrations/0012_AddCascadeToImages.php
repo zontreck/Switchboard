e2d02887127e86d50ea7964d32837f35053a5f2b
Zontreck <tarapiccari@gmail.com>
2026-04-13 19:20:39 -0700

    Patch typo in migration 12

--- server/migrations/0012_AddCascadeToImages.php
+++ server/migrations/0012_AddCascadeToImages.php
@@ -1,7 +1,7 @@
 <?php
 
 return function($conn) {
-    $conn->query("ALTER TABLE `Images` ADD CONSTRAINT `UID` FOREIGN KEY (`OwnerID`) REFERENCES `users`(`ID`) ON DELETE CASCADE ON UPDATE CASCADE;");
+    $conn->query("ALTER TABLE `Images` ADD FOREIGN KEY (`OwnerID`) REFERENCES `users`(`ID`) ON DELETE CASCADE ON UPDATE CASCADE;");
 }
 
 ?>
