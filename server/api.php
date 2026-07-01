f31c0c1d94687bc4c3de79a8ea2e2a357d369beb
zontreck <tara@zontreck.com>
2026-07-01 15:33:30 -0700

    Attempt to fix and resolve new user account creation bugs

--- server/api.php
+++ server/api.php
@@ -2,7 +2,7 @@
 
 $DEBUG = false;
 
-$VERSION = "0.2.0+0701261501";
+$VERSION = "0.3.0+0701261533";
 
 $DEFAULT_USER_FIELDS = array(
                             array(
@@ -368,7 +368,7 @@ switch($route) {
                     foreach ($DEFAULT_USER_FIELDS as $value) {
                         // Create the default fields for the specified user.
                         $fieldID = gen_uuid();
-                        $dfStmt = $DB->prepare("INSERT INTO Fields (User, ID, FieldName, FieldType, Order) VALUES (?,?,?,?, ?);");
+                        $dfStmt = $DB->prepare("INSERT INTO `Fields` (`User`, `ID`, `FieldName`, `FieldType`, `Order`) VALUES (?,?,?,?, ?);");
                         $dfStmt->bind_param("sssi", $userid, $fieldID, $value["Name"], $value["Type"], $value["Order"]);
                         $dfStmt->execute();
                         $dfStmt->close();
