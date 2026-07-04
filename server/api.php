48361e9098270b9985de1ac9ae1654ee71c23e09
zontreck <tara@zontreck.com>
2026-07-04 15:31:40 -0700

    Fix syntax error in migration 7

--- server/api.php
+++ server/api.php
@@ -2,7 +2,7 @@
 
 $DEBUG = false;
 
-$VERSION = "0.3.0+0704261511";
+$VERSION = "0.3.0+0704261531";
 
 $DEFAULT_USER_FIELDS = array(
                             array(
--- server/migrations/0007_AdjustFKFolders.php
+++ server/migrations/0007_AdjustFKFolders.php
@@ -3,8 +3,8 @@
 return function($conn) {
     try {
         $conn->query("ALTER TABLE `Folders` DROP FOREIGN KEY `FK_Folders_Parent`;");
-    }catch($E) {}
-    
+    }catch(Exception $E) {}
+
     $conn->query("ALTER TABLE `Folders` ADD CONSTRAINT `FK_Folders_Parent` FOREIGN KEY (`ParentFolder`) REFERENCES `Folders`(`ID`) ON DELETE SET NULL ON UPDATE CASCADE;")
 }
 
