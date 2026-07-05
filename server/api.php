c430fc5822f2eba9cf276b307482ef36da6eda57
zontreck <tara@zontreck.com>
2026-07-04 17:06:55 -0700

    Fix missing semi-colon

--- server/api.php
+++ server/api.php
@@ -2,7 +2,7 @@
 
 $DEBUG = false;
 
-$VERSION = "0.3.0+0704261531";
+$VERSION = "0.3.0+0704261706";
 
 $DEFAULT_USER_FIELDS = array(
                             array(
--- server/migrations/0007_AdjustFKFolders.php
+++ server/migrations/0007_AdjustFKFolders.php
@@ -5,7 +5,7 @@ return function($conn) {
         $conn->query("ALTER TABLE `Folders` DROP FOREIGN KEY `FK_Folders_Parent`;");
     }catch(Exception $E) {}
 
-    $conn->query("ALTER TABLE `Folders` ADD CONSTRAINT `FK_Folders_Parent` FOREIGN KEY (`ParentFolder`) REFERENCES `Folders`(`ID`) ON DELETE SET NULL ON UPDATE CASCADE;")
+    $conn->query("ALTER TABLE `Folders` ADD CONSTRAINT `FK_Folders_Parent` FOREIGN KEY (`ParentFolder`) REFERENCES `Folders`(`ID`) ON DELETE SET NULL ON UPDATE CASCADE;");
 }
 
 ?>
