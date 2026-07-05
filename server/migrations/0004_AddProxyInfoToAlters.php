844d72205125be1baddd0cfecee5f655491dec7d
zontreck <tara@zontreck.com>
2026-07-04 23:03:58 -0700

    Fix syntax error

--- server/migrations/0004_AddProxyInfoToAlters.php
+++ server/migrations/0004_AddProxyInfoToAlters.php
@@ -4,3 +4,5 @@ return function($conn) {
     $conn->query("ALTER TABLE `Alters` ADD ProxyName VARCHAR(64) NOT NULL DEFAULT '' AFTER Flags;");
     $conn->query("ALTER TABLE `Alters` ADD Proxies TEXT NULL AFTER ProxyName;");
 }
+
+?>
