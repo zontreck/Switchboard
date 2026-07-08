01802cd679bd58b7a9c8ae8365fbf85d7b5be882
zontreck <tara@zontreck.com>
2026-07-08 09:35:54 -0700

    Rollback backend change

--- server/api.php
+++ server/api.php
@@ -2,7 +2,7 @@
 
 $DEBUG = false;
 
-$VERSION = "0.3.1+0708260928";
+$VERSION = "0.3.1+0708260935";
 
 $DEFAULT_USER_FIELDS = array(
                             array(
@@ -843,7 +843,7 @@ switch($route) {
                     $rres = $stmt->get_result();
                 }
 
-                if($rres->num_rows == 0) {
+                if($rres->num_rows < $AlterCount) {
                     header("X-SB-Done: 1");
                     $reason = "DONE";
                 }
